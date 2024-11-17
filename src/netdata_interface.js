e64cb7d601a68c460a4ad92209790e5f3d30fff1
Marc <bolverblitz@ebg.pw>
2024-11-17 11:51:02 +0100

    Netdata Charts

--- src/netdata_interface.js
+++ src/netdata_interface.js
@@ -2,44 +2,54 @@
 const HyperExpress = require('hyper-express');
 // const { writeOverwriteCacheKey } = require('@lib/cache');
 const { gzipCompress, gzipDecompress } = require('@lib/object_compressor');
-const { GetNetdataMonitor } = require('@lib/redis');
+const { GetNetdataMonitor, SaveNetdataNodeChart } = require('@lib/redis');
 const { parseNetdata } = require('@lib/netdata_parser');
 const NetdataCharts = require('@lib/netdata_charts');
 const router = new HyperExpress.Router();
 
 router.post('/put', /* limiter(1) ,*/ async (req, res) => {
-    const parsedData = await parseNetdata(await req.json());
-    const reqIP = req.headers['x-forwarded-for'] || req.ip;
-    console.log(`Received data from ${reqIP}`);
-    const monitor_config = await GetNetdataMonitor(parsedData.hostname, reqIP);
-    const chart_fields = NetdataCharts.listCharts(monitor_config.charts);
-    console.log(monitor_config, chart_fields);
-    chart_fields.forEach(async (chart) => {
-        console.log(`Saving chart ${chart} for ${parsedData.hostname}`, NetdataCharts.getChartFields(chart));
+  const parsedData = await parseNetdata(await req.json());
+  const reqIP = req.headers['x-forwarded-for'] || req.ip;
+  console.log(`Received data from ${reqIP}`);
+  const monitor_config = await GetNetdataMonitor(parsedData.hostname, reqIP);
+  const chart_fields = NetdataCharts.listCharts(monitor_config.charts); // Convert the bit-mask to a list of chart fields
+  console.log(monitor_config, chart_fields);
+
+  chart_fields.forEach(async (chart) => {
+    const metricNamesForChart = NetdataCharts.getChartFields(chart);
+    process.log.debug(`Saving chart ${chart} for ${parsedData.hostname}: ${metricNamesForChart.join(', ')}`);
+
+    metricNamesForChart.forEach(async (metric, index) => {
+      const chartData = parsedData.metrics[metric];
+      if (chartData) {
+        await SaveNetdataNodeChart(parsedData.hostname, reqIP, chart, index, monitor_config.chart_hours, chartData.value);
+      }
     });
+  });
 
-    // Validate IP address and Hostname
-    // process.log.debug(`Hostname: ${parsedData.hostname}`);
 
-    // Measure compression and decompression for gzip
-    console.time("Gzip Compression");
-    const compressedData_b = gzipCompress(parsedData);
-    console.timeEnd("Gzip Compression");
+  // Validate IP address and Hostname
+  // process.log.debug(`Hostname: ${parsedData.hostname}`);
 
-    console.time("Gzip Decompression");
-    const decompressedData_b = gzipDecompress(compressedData_b);
-    console.timeEnd("Gzip Decompression");
+  // Measure compression and decompression for gzip
+  console.time("Gzip Compression");
+  const compressedData_b = gzipCompress(parsedData);
+  console.timeEnd("Gzip Compression");
 
-    // Output sizes for comparison
-    console.log("Original Data Size:", Buffer.byteLength(JSON.stringify(parsedData), 'utf8'), "bytes");
-    console.log("Gzip Compressed Data Size:", compressedData_b.length, "bytes");
-    console.log("Decompressed Data Size (Gzip):", Buffer.byteLength(JSON.stringify(decompressedData_b), 'utf8'), "bytes");
-    console.log("Storrage Size Avaible:", decompressedData_b.metrics["disk.space.avail"].value, decompressedData_b.metrics["disk.space.avail"].units);
+  console.time("Gzip Decompression");
+  const decompressedData_b = gzipDecompress(compressedData_b);
+  console.timeEnd("Gzip Decompression");
 
-    // console.log(JSON.stringify(parsedData, null, 2));
+  // Output sizes for comparison
+  console.log("Original Data Size:", Buffer.byteLength(JSON.stringify(parsedData), 'utf8'), "bytes");
+  console.log("Gzip Compressed Data Size:", compressedData_b.length, "bytes");
+  console.log("Decompressed Data Size (Gzip):", Buffer.byteLength(JSON.stringify(decompressedData_b), 'utf8'), "bytes");
+  console.log("Storrage Size Avaible:", decompressedData_b.metrics["disk.space.avail"].value, decompressedData_b.metrics["disk.space.avail"].units);
 
-    // Send a response back to Netdata
-    res.status(200).send(JSON.stringify(parsedData));
+  // console.log(JSON.stringify(parsedData, null, 2));
+
+  // Send a response back to Netdata
+  res.status(200).send(JSON.stringify(parsedData));
 });
 
 module.exports = router;
